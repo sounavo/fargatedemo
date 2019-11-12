b54f69f1e98da63bd46550d6309b85a79988a1d1
Sounavo <sounavo@amazon.com>
2019-11-12 16:15:38 +0100

    Add cleanup script

---
+++ cloudformation/cleanup.sh
@@ -0,0 +1,3 @@
+aws cloudformation delete-stack  --stack-name fargate-demo-chat-app
+aws cloudformation delete-stack  --stack-name fargate-demo-cluster
+aws cloudformation delete-stack  --stack-name fargate-demo-vpc
